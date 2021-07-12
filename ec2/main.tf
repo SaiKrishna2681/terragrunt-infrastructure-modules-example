f2cdcf17779a22507127133eeeb8b36bdb7cd20e
kethireddy114 <85087104+kethireddy114@users.noreply.github.com>
2021-07-12 11:59:04 +0530

    terraform
    adding terraform

---
+++ ec2/main.tf
@@ -0,0 +1,18 @@
+terraform {
+  # Live modules pin exact Terraform version; generic modules let consumers pin the version.
+  # The latest version of Terragrunt (v0.29.0 and above) recommends Terraform 0.15.0 or above.
+  required_version = "= 0.15.0"
+
+  # Live modules pin exact provider version; generic modules let consumers pin the version.
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "= 3.7.0"
+    }
+  }
+}
+
+resource "aws_instance" "thedbadmin_test" {
+  ami = var.ami
+  instance_type = var.type
+}
